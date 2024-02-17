6e15c58ed45d54cdcce7b76b543af42d933c0b34
arthur <arthurazevedods@gmail.com>
2024-02-17 00:22:40 -0300

    first with errors

---
+++ index.js
@@ -0,0 +1,23 @@
+const { Socket } = require('socket.io');
+
+const io = require('socket.io')(8000, {cors:{origin:"*"}});
+
+let totalVotes = 0;
+let votingPolls = {
+    'dont-like': 0,
+    'like': 0,
+}
+
+io.on('connection',socket =>{
+
+    //send current data of votes to user when visited the site
+    socket.emit('update',{votingPolls,totalVotes})
+    
+    socket.on('send-vote', voteTo =>{
+        totalVotes +=1;
+        //console.log(voteTo);
+        votingPolls[voteTo] +=1;
+        socket.broadcast.emit('receive-vote',{votingPolls,totalVotes});
+        socket.emit('update',{votingPolls,totalVotes})
+    })
+})

be8aeace1485a198b103dc943caa26787102bc99
arthur <arthurazevedods@gmail.com>
2024-03-05 12:13:13 -0300

    headers app

--- app.js
+++ app.js
@@ -14,15 +14,8 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 app.use(function(req, res, next) {
-  // res.header("Access-Control-Allow-Origin", "*");
-  const allowedOrigins = ['http://localhost:3000', 'https://poll-front-end-six.vercel.app' ,'https://supervisao-e-sinergia.vercel.app', 'https://poll-nodeserver.onrender.com'];
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-       res.setHeader('Access-Control-Allow-Origin', origin);
-  }
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  res.header("Access-Control-Allow-credentials", true);
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+  res.header("Access-Control-Allow-Origin", "https://seusite.com"); // substitua "https://seusite.com" pelo seu domínio
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 

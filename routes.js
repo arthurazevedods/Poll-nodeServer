33ed13ad7cdedea345da47649f1d361eae6a4aca
arthur <arthurazevedods@gmail.com>
2024-03-05 12:18:08 -0300

    fix routes

--- routes.js
+++ routes.js
@@ -4,50 +4,51 @@ const router = express.Router()
 
 //Routes
 
-router.get('/enquets', async (req, res) =>{
+router.get('/enquets', async (req, res) => {
 
     try {
-        const result  = await admin.getFirestore().collection('wishes').get()
+        const result = await admin.getFirestore().collection('wishes').get()
         const list = result.docs.map(item => ({
             ...item.data(),
             uid: item.id
         }))
 
-       return res.json(list)
-    }catch{
+        return res.json(list)
+    } catch {
         return res.json("Erro Api")
     }
 })
 
-router.get('/enquets/:id', async (req, res) =>{
-   const id = req.params.id
-   try{
+router.get('/enquets/:id', async (req, res) => {
+    const id = req.params.id
+    try {
         const result = await admin.getFirestore().collection('wishes').doc(id).get()
-        
-        if(!result.exists){
+
+        if (!result.exists) {
             return res.json("Documento não encontrado")
-        }else{
+        } else {
             return res.json(result.data())
         }
 
-    }catch{
+    } catch {
         return res.json("Erro na API")
     }
-   
-})  
+
+})
 
 router.put('/enquets/:id', async (req, res) => {
-   try{
+    try {
+        const id = req.params.id; // Adicione esta linha para extrair o ID do parâmetro
         console.log("api data" + req.body.like)
         await admin.getFirestore().collection('wishes').doc(id).update({
-            votesLike : req.body.like,
+            votesLike: req.body.like,
             votesUnlike: req.body.unlike
         })
 
-   }catch{
-        return res.json("Erro Api")
-   }
-
+    } catch (error) {
+        console.error("Erro ao atualizar enquete:", error);
+        return res.status(500).json({ error: "Erro na API" });
+    }
 })
 
 

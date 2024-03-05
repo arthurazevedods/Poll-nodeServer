c91f87a6a2ff952a0478f6b2fb6f0712dcbfc46b
arthur <arthurazevedods@gmail.com>
2024-03-05 12:21:03 -0300

    localhost

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://supervisao-e-sinergia.vercel.app, http://localhost:5500");
+  res.header("Access-Control-Allow-Origin", "https://supervisao-e-sinergia.vercel.app, http://localhost:5500, http://127.0.0.1:5500");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
